3c1064122fc2b02fd1b35fd7b2d40bf489a82a2d
ShishirBhandari <shishirbhandari54@gmail.com>
2020-07-15 21:55:32 +0545

    Updates

---
+++ precache-manifest.4e52505b83e1162275d967672af0d7f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d05e12e4c2e724f69816da82a7d74",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "cde60c5d884ed29850ec",
+    "url": "/react-todo-app/static/css/main.f4285a65.chunk.css"
+  },
+  {
+    "revision": "06e88e246470ef86daff",
+    "url": "/react-todo-app/static/js/2.d0eea4ca.chunk.js"
+  },
+  {
+    "revision": "cde60c5d884ed29850ec",
+    "url": "/react-todo-app/static/js/main.a78a8333.chunk.js"
+  },
+  {
+    "revision": "f17a649cf56d1e44d3f3",
+    "url": "/react-todo-app/static/js/runtime-main.0c8055af.js"
+  }
+]);
